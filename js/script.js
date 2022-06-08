f34d96e370319c77849319e672a24c41748fe89f
Regina Santangelo <reggiisantangelo@gmail.com>
2022-06-08 15:06:41 -0300

    localStorage

--- js/script.js
+++ js/script.js
@@ -1,79 +1,56 @@
-class carrito {
-    constructor(producto, precio) {
-        this.producto = producto;
-        this.precio = precio;
+let carts = document.querySelectorAll('.cart');
+
+let products = [
+    {
+        name:'Masaje descontraturante',
+        precio: 1000,
+        inCart: 0
+    },
+    {
+        name:'Pediluvios',
+        precio: 700,
+        inCart: 0
+    },
+    {
+        name:'Linfaticos faciales',
+        precio: 870,
+        inCart: 0
+    },
+    {
+        name:'Reductores',
+        precio: 1200,
+        inCart: 0
+    }
+]
+
+for(let i=0; i < carts.length; i++){
+    carts[i].addEventListener ('click', () => {
+        cartNumbers();
+    })
+}
+
+function cartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers, +1);
+
+    }else{
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.card span').textContent = 1;
     }
 }
 
-const carrito1= new carrito("crema", 500);
-const carrito2 = new carrito("exfoliante", 600);
-const carrito3 = new carrito ("crema para masajes", 750);
-
-// let opcion = prompt("Elegi un producto: 1 Crema-500. \n 2 Exfoliante-600. \n 3 Crema para masajes-725. \n 4 Mascarilla Facial \n Presiona X para cancelar la operacion")
-
-// while (opcion != 'X'){
-//     switch (opcion) {
-//     case '1':
-//         let cantidad = parseFloat(prompt("Ingresa la cantidad"));
-//         if(cantidad > 1){
-//             resultado = 500 * cantidad;
-//             alert("El total de tu compra es:" + resultado)
-//         } else{
-//             alert("introducí un numero valido -1 o más-")
-//         }
-//         break;
-
-//     case '2':
-//         let cantidad2 = parseFloat(prompt("Ingresa la cantidad"));
-
-//         if(cantidad2 > 1){
-//             resultado= 600 * cantidad2
-//             alert("El total de tu compra es:" + resultado);
-//         } else{
-//             alert("introducí un numero valido -1 o más-")
-//         }
-//         break;
-    
-//     case '3':
-//         let cantidad3 = parseFloat(prompt("Ingresa la cantidad"));
-//         if(cantidad3 > 1){
-//             resultado= 750 * cantidad3
-//             alert("El total de tu compra es:" + resultado);
-//         } else{
-//             alert("introducí un numero valido -1 o más-")
-//         }
-//         break;
-
-//     case "4":
-//         alert("Sin stock")
-//     break;
-
-//     default:
-//         break;
-//     }
-//  alert("Gracias por tu compra"); break;
-// }
-
-const productos = ["crema","exfoliante","crema para masajes"];
-console.log(productos.length)
-
-productos.push(new carrito("Mascarilla facial", 300));
-console.log(productos.length)
+console.log(carts.length);
 
 //APLICANDO DOM
 
-let titulo = document.getElementById('tituloProducto');
-if(titulo){
-    titulo.innerText = prompt("Introducí un nuevo nombre para el título");
-    console.log(titulo.innerText); 
-}
-
-let productoDom = {
-    id: 1,
-    nombre: 'Crema',
-    aroma: 'Lavanda',
-    precio1: 500
-};
+// let titulo = document.getElementById('tituloProducto');
+// if(titulo){
+//     titulo.innerText = prompt("Introducí un nuevo nombre para el título");
+//     console.log(titulo.innerText); 
+// }
 
 // let plantilla = `ID:${productoDom.id} \nProducto ${productoDom.nombre} \nAroma:${productoDom.aroma} \nPrecio:${productoDom.precio1}`;
 
@@ -91,7 +68,8 @@ let productoDom = {
 
 const formulario = document.getElementById('form');
 
-formulario.addEventListener('submit', function(e){
+if(formulario){
+    formulario.addEventListener('submit', function(e){
     e.preventDefault();
     let inputUno = document.getElementById('input1').value;
     console.log(inputUno);
@@ -100,8 +78,10 @@ formulario.addEventListener('submit', function(e){
     let inputTres = document.getElementById('input3').value;
     console.log(inputTres)
 })
-
 let enviar = document.getElementById('enviar');
 enviar.onclick = () =>{
     alert("Gracias por tu mensaje ,en breve te estaremos contactando.");
 } 
+}
+
+
