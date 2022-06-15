f8aa1978bacded4acb1437baa297838e40f732f5
Regina Santangelo <reggiisantangelo@gmail.com>
2022-06-15 20:29:13 -0300

    Agregué un sweetalert en el form

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
-let carts = document.querySelectorAll('.boton');
+let carts = document.querySelectorAll('.comprar');
 
 let products = [
     {
-        name:'Masaje descontraturante',
+        id:1,
+        name:'Aceite esencial de rosas',
+        precio: 720,
+        inCart: 0
+    },
+    {
+        id:2,
+        name:'Crema de manzanilla',
         precio: 1000,
         inCart: 0
     },
     {
-        name:'Pediluvios',
+        id:3,
+        name:'Aceite esencial de jazmín',
         precio: 700,
-        inCart: 0
-    },
-    {
-        name:'Linfaticos faciales',
-        precio: 870,
-        inCart: 0
-    },
-    {
-        name:'Reductores',
-        precio: 1200,
         inCart: 0
     }
 ]
@@ -30,45 +28,29 @@ for(let i=0; i < carts.length; i++){
     })
 }
 
-function cartNumbers(product){
-    let productNumbers = parseInt(localStorage.getItem('cartNumbers'));
-    let numeroCarrito = document.querySelector('.numCarrito');
-    
+function guardadoEnCarrito (){
+    let productNumbers = localStorage.getItem('cantidad');
+
     if(productNumbers){
-        localStorage.setItem('cartNumbers', productNumbers + 1);
-        numeroCarrito.innerHTML = productNumbers + 1;
-
-    }else{
-        localStorage.setItem('cartNumbers', 1);
-        document.querySelector('.cart').textContent = 1;
-        numeroCarrito.innerHTML = 1;
+        document.querySelector('.numCarrito').textContent = productNumbers;
     }
-
-    setItem(product);
 }
 
-function setItem(product) {
-    let carritoProducts = localStorage.getItem('Productos');
+function cartNumbers (producto) {
+    console.log('el producto es', producto);
+    let productNumbers = localStorage.getItem('cantidad');
+    productNumbers = parseInt(productNumbers);
 
-    if(carritoProducts != null){
-        carritoProducts[product.name].inCart += 1;
-    }else{
-        product.inCart = 1;
-        carritoProducts = {
-            [product.name]: product
-        }
+    if (productNumbers){
+        localStorage.setItem('cantidad', productNumbers + 1);
+        document.querySelector('.numCarrito').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cantidad', 1);
+        document.querySelector('.numCarrito').textContent =  1;
     }
-
-    localStorage.setItem("Productos", JSON.stringify(carritoProducts));
 }
 
-//APLICANDO DOM
-
-// let titulo = document.getElementById('tituloProducto');
-// if(titulo){
-//     titulo.innerText = prompt("Introducí un nuevo nombre para el título");
-//     console.log(titulo.innerText); 
-// }
+guardadoEnCarrito();
 
 // let plantilla = `ID:${productoDom.id} \nProducto ${productoDom.nombre} \nAroma:${productoDom.aroma} \nPrecio:${productoDom.precio1}`;
 
@@ -83,23 +65,35 @@ function setItem(product) {
 // nuevaCard.className = "card"
 
 //Aplicando Eventos (en formulario de contacto)
+//----------------------------------------------------
 
 const formulario = document.getElementById('form');
 
 if(formulario){
     formulario.addEventListener('submit', function(e){
     e.preventDefault();
-    let inputUno = document.getElementById('input1').value;
+    let inputUno = document.getElementById('input1').value= this.ariaPlaceholder;
     console.log(inputUno);
-    let inputDos = document.getElementById('input2').value;
-    console.log(inputDos)
-    let inputTres = document.getElementById('input3').value;
-    console.log(inputTres)
+    let inputDos = document.getElementById('input2').value=this.ariaPlaceholder;
+    console.log(inputDos);
+    let inputTres = document.getElementById('input3').value=this.ariaPlaceholder;
+    console.log(inputTres);
+    
 })
+
 let enviar = document.getElementById('enviar');
-enviar.onclick = () =>{
-    alert("Gracias por tu mensaje ,en breve te estaremos contactando.");
-} 
+
+enviar.addEventListener('click', ()=>{
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Tu mensaje fué enviado, en breve te estaremos contactando!',
+        showConfirmButton: false,
+        timer: 2500
+      })
+})
+reset(inputUno.value);
+reset(inputTres.value);
 }
 
 
